48ae1e81cf1b26d6f9ebd50a5c793f42effbfb99
Adam Beighley <Adam035827@gmail.com>
2018-04-15 10:44:29 -0500

    boilerplate code and navbar navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { PrivatePolicyComponent } from './private-policy/private-policy.component';
+import { TermsOfServiceComponent } from './terms-of-service/terms-of-service.component';
 
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'private-policy', component: PrivatePolicyComponent },
+  { path: 'terms-of-service', component: TermsOfServiceComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ProjectsComponent,
+    PrivatePolicyComponent,
+    TermsOfServiceComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes, {enableTracing: true}
+    ),
     BrowserModule
   ],
   providers: [],
