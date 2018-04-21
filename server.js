30b70735707040725083a888e3744c98c054a4d1
Adam <Adam035827@gmail.com>
2018-04-21 07:57:22 -0500

    testing mail server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ console.log('called server.js');
 
 app.use(express.static(__dirname + '/dist'));
 
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 8080);
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
