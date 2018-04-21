97c9e0f64d77326bcfc877eb4706eee43bb56e06
Adam <Adam035827@gmail.com>
2018-04-21 08:05:57 -0500

    testing mail server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express();
-const path = require('path')
+const path = require('path');
+var nodemailer = require('nodemailer');
+
+console.log('called server.js');
 
 app.use(express.static(__dirname + '/dist'));
 
@@ -10,5 +13,30 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-console.log('console listening');
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'adam035827@gmail.com',
+      pass: 'Linkin12'
+    }
+  });
+  
+  console.log('created transport');
+
+  var mailOptions = {
+    from: 'adam035827@gmail.com',
+    to: 'adam035827@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: 'That was easy!'
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+
+  console.log('sent mail');
 
